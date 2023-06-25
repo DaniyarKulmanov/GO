957be22d27fdd4f6df7e4719531507cfbec9b410
daniyar kulmanov <daniyarkulmanov@iMac--BTS-Kulmanov.local>
2023-06-25 16:01:24 +0600

    demoapp

--- 01-lesson/demoapp/cmd/app/main.go
+++ 01-lesson/demoapp/cmd/app/main.go
@@ -1,5 +1,19 @@
 package main
 
-func main() {
+import (
+	"GO_thinknetica/01-lesson/demoapp/pkg/strings_utils"
+	"net/http"
+)
 
+func main() {
+	http.ListenAndServe(
+		":8181",
+		http.HandlerFunc(
+			func(w http.ResponseWriter, r *http.Request) {
+				s := r.URL.Query()["s"]
+				rev := strings_utils.Rev(s)
+				w.Write([]byte(rev))
+			},
+		),
+	)
 }
