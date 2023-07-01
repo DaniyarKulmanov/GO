18f8db99c0fa3e84e4b7cb23c28d8d61c215b47d
daniyar kulmanov <daniyarkulmanov@iMac.local>
2023-07-01 18:30:37 +0600

    goSearch3

--- GoSearch/cmd/gosearch/gosearch.go
+++ GoSearch/cmd/gosearch/gosearch.go
@@ -7,7 +7,6 @@ import (
 )
 
 func main() {
-	// create slice for data
 	var data []crawler.Document
 	sts := [2]string{
 		"https://go.dev/",
@@ -15,7 +14,6 @@ func main() {
 	}
 	s := spider.New()
 
-	// scan sites
 	for i := range sts {
 		result, err := s.Scan(sts[i], 1)
 
@@ -23,11 +21,8 @@ func main() {
 			fmt.Println(err)
 			continue
 		}
-		// result append to data
 		data = append(data, result...)
-		//print section
-		//fmt.Println(result)
 	}
-	// write data to a file
+
 	fmt.Println(data)
 }
