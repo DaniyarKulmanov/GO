aa9d59d7c0b01490b741134df300434190080c9b
daniyar kulmanov <daniyarkulmanov@iMac.local>
2023-07-01 14:17:51 +0600

    gosearch working variant

--- GoSearch/cmd/gosearch/gosearch.go
+++ GoSearch/cmd/gosearch/gosearch.go
@@ -1,22 +1,33 @@
 package main
 
 import (
+	"GO_thinknetica/GoSearch/pkg/crawler"
 	"GO_thinknetica/GoSearch/pkg/crawler/spider"
 	"fmt"
-	"reflect"
 )
 
 func main() {
 	// create slice for data
-	// scan sites
-	// iterate through result and append to data
-	// write data to a file
-
+	var data []crawler.Document
+	sts := [2]string{
+		"https://go.dev/",
+		"https://www.programiz.com/golang/",
+	}
 	s := spider.New()
-	result, err := s.Scan("https://go.dev/", 1)
-	//result, err := s.Scan("https://www.programiz.com/golang/struct", 1)
-	fmt.Println(reflect.TypeOf(result[0]))
-	//fmt.Println(result)
-	fmt.Printf("len=%d cap=%d %v\n", len(result), cap(result), result)
-	fmt.Println(err)
+
+	// scan sites
+	for i := range sts {
+		result, err := s.Scan(sts[i], 1)
+
+		if err != nil {
+			fmt.Println(err)
+			continue
+		}
+		// result append to data
+		data = append(result)
+		//print section
+		fmt.Println(result)
+	}
+	// write data to a file
+	fmt.Println(data)
 }
