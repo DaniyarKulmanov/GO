a9942985d2a20aeca79214549e7a56cd66c44a52
daniyar kulmanov <DanRubyDeveloper@gmail.com>
2023-07-01 21:22:42 +0600

    finish work

--- GoSearch/cmd/gosearch/gosearch.go
+++ GoSearch/cmd/gosearch/gosearch.go
@@ -10,11 +10,14 @@ import (
 
 func main() {
 	var data []crawler.Document
+	var search string
 	sts := [2]string{
 		"https://go.dev/",
 		"https://www.programiz.com/golang/",
 	}
 	s := spider.New()
+	flag.StringVar(&search, "s", "", "search links")
+	flag.Parse()
 
 	for i := range sts {
 		result, err := s.Scan(sts[i], 1)
@@ -23,27 +26,14 @@ func main() {
 			fmt.Println(err)
 			continue
 		}
-		data = append(data, result...)
-	}
 
-	// read flag
-	var search string
-
-	flag.StringVar(&search, "s", "", "search links")
-	flag.Parse()
-
-	// filter content based on search
-	var dataFilter []crawler.Document
-	for i := range data {
-		if strings.Contains(data[i].URL, search) {
-			dataFilter = append(dataFilter, data[i])
+		if search != "" {
+			if strings.Contains(sts[i], search) {
+				data = append(data, result...)
+			}
+		} else {
+			data = append(data, result...)
 		}
 	}
-
-	if search != "" {
-		fmt.Println(dataFilter)
-	} else {
-		fmt.Println(data)
-	}
-
+	fmt.Println(data)
 }
